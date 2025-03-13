e8c23ba083e3cf5b4f9196c263419c724dafd9f8
Yamin-402 <140313944+Yamin-402@users.noreply.github.com>
2025-03-13 14:56:03 +0200

    removing /data v2

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const bcrypt = require('bcryptjs');
 const path = require('path');
 const moment = require('moment-timezone');
 const app = express();
-// Use an absolute path for SQLite (Fly.io uses `/app` as the root directory)
-const DB_FILE = path.join(__dirname,'database.db'); // Now in /app/data
+const DB_FILE = path.join(__dirname, 'database.db');
 const db = new sqlite3.Database(DB_FILE);
 // ramadan
 const ramadanStart = moment.tz("2025-03-01", "YYYY-MM-DD", "Africa/Cairo"); // تاريخ بداية رمضان 2024
